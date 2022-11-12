abb7a16877fdce42a3fd0d495fa9b3efc4ea8465
serwisgalena <serwisgalena@gmail.com>
2022-11-12 13:10:20 +0100

    constants

--- packages/core/src/utils/blockRewards.ts
+++ packages/core/src/utils/blockRewards.ts
@@ -7,52 +7,40 @@ const FARMER_REWARD = '0.125'; // 1 /8
 
 export function calculatePoolReward(height: number): BigNumber {
   if (height === 0) {
-    return MOJO_PER_CHIA.times('20000000').times(POOL_REWARD);
+    return MOJO_PER_CHIA.times('3411000').times(POOL_REWARD);
   }
-  if (height < 1000000) {
-    return MOJO_PER_CHIA.times('200').times(POOL_REWARD);
+  if (height < 1 * BLOCKS_PER_YEAR) {
+    return MOJO_PER_CHIA.times('2').times(POOL_REWARD);
   }
-  if (height < 1000000 + (3 * BLOCKS_PER_YEAR)) {
-    return MOJO_PER_CHIA.times('20').times(POOL_REWARD);
+  if (height < 2 * BLOCKS_PER_YEAR) {
+    return MOJO_PER_CHIA.times('1').times(POOL_REWARD);
   }
-  if (height < 1000000 + (6 * BLOCKS_PER_YEAR)) {
-    return MOJO_PER_CHIA.times('10').times(POOL_REWARD);
+  if (height < 3 * BLOCKS_PER_YEAR) {
+    return MOJO_PER_CHIA.times('0.5').times(POOL_REWARD);
   }
-  if (height < 1000000 + (9 * BLOCKS_PER_YEAR)) {
-    return MOJO_PER_CHIA.times('5').times(POOL_REWARD);
-  }
-  if (height < 1000000 + (12 * BLOCKS_PER_YEAR)) {
-    return MOJO_PER_CHIA.times('2.5').times(POOL_REWARD);
-  }
-  if (height < 1000000 + (15 * BLOCKS_PER_YEAR)) {
-    return MOJO_PER_CHIA.times('1.25').times(POOL_REWARD);
+  if (height < 4 * BLOCKS_PER_YEAR) {
+    return MOJO_PER_CHIA.times('0.25').times(POOL_REWARD);
   }
 
-  return new BigNumber(0);
+  return MOJO_PER_CHIA.times('0.125').times(POOL_REWARD);
 }
 
 export function calculateBaseFarmerReward(height: number): BigNumber {
   if (height === 0) {
-    return MOJO_PER_CHIA.times('20000000').times(FARMER_REWARD);
+    return MOJO_PER_CHIA.times('3411000').times(FARMER_REWARD);
   }
-  if (height < 1000000 + (3 * BLOCKS_PER_YEAR)) {
-    return MOJO_PER_CHIA.times('200').times(FARMER_REWARD);
+  if (height < 1 * BLOCKS_PER_YEAR) {
+    return MOJO_PER_CHIA.times('2').times(FARMER_REWARD);
   }
-  if (height < 1000000 + (6 * BLOCKS_PER_YEAR)) {
-    return MOJO_PER_CHIA.times('20').times(FARMER_REWARD);
+  if (height < 2 * BLOCKS_PER_YEAR) {
+    return MOJO_PER_CHIA.times('1').times(FARMER_REWARD);
   }
-  if (height < 1000000 + (9 * BLOCKS_PER_YEAR)) {
-    return MOJO_PER_CHIA.times('10').times(FARMER_REWARD);
+  if (height < 3 * BLOCKS_PER_YEAR) {
+    return MOJO_PER_CHIA.times('0.5').times(FARMER_REWARD);
   }
-  if (height < 1000000 + (12 * BLOCKS_PER_YEAR)) {
-    return MOJO_PER_CHIA.times('5').times(FARMER_REWARD);
-  }
-  if (height < 1000000 + (12 * BLOCKS_PER_YEAR)) {
-    return MOJO_PER_CHIA.times('2.5').times(FARMER_REWARD);
-  }
-  if (height < 1000000 + (15 * BLOCKS_PER_YEAR)) {
-    return MOJO_PER_CHIA.times('1.25').times(FARMER_REWARD);
+  if (height < 4 * BLOCKS_PER_YEAR) {
+    return MOJO_PER_CHIA.times('0.25').times(FARMER_REWARD);
   }
 
-  return new BigNumber(0);
+  return MOJO_PER_CHIA.times('0.125').times(FARMER_REWARD);
 }
