bbd955faeb9a9a4873bd30835b1d4d3876d4ed87
serwisgalena <serwisgalena@gmail.com>
2022-11-12 13:27:08 +0100

    constants

--- packages/core/src/utils/blockRewards.ts
+++ packages/core/src/utils/blockRewards.ts
@@ -7,7 +7,7 @@ const FARMER_REWARD = '0.125'; // 1 /8
 
 export function calculatePoolReward(height: number): BigNumber {
   if (height === 0) {
-    return MOJO_PER_CHIA.times('3411000').times(POOL_REWARD);
+    return MOJO_PER_CHIA.times('6822144').times(POOL_REWARD);
   }
   if (height < 1 * BLOCKS_PER_YEAR) {
     return MOJO_PER_CHIA.times('2').times(POOL_REWARD);
@@ -27,7 +27,7 @@ export function calculatePoolReward(height: number): BigNumber {
 
 export function calculateBaseFarmerReward(height: number): BigNumber {
   if (height === 0) {
-    return MOJO_PER_CHIA.times('3411000').times(FARMER_REWARD);
+    return MOJO_PER_CHIA.times('6822144').times(FARMER_REWARD);
   }
   if (height < 1 * BLOCKS_PER_YEAR) {
     return MOJO_PER_CHIA.times('2').times(FARMER_REWARD);
